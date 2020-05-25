b96ab647e27ab553393041e033bea7258686eb51
Mariia Turchina <mariia.turchina@gmail.com>
2020-05-25 12:47:55 +0200

    added audio synthesis for nonexisting audio files

--- PiaEnglish/Collections/Word.swift
+++ PiaEnglish/Collections/Word.swift
@@ -33,6 +33,14 @@ func get_audio_path(of word: String) -> String{
     return "\(word).m4a"
 }
 
+func synthesize_audio(of text: String) {
+    let utterance = AVSpeechUtterance(string: text)
+    utterance.voice = AVSpeechSynthesisVoice(language: "en-US")
+    let synthesizer = AVSpeechSynthesizer()
+    synthesizer.speak(utterance)
+    utterance.rate = 0.1
+}
+
 func play_audio_of(word: String) {
     
     let path = get_absolute_audio_path(of: word)
@@ -40,31 +48,33 @@ func play_audio_of(word: String) {
     
     let ref = storage.reference()
     
-    // Create a reference to the file you want to download
+    /// Create a reference to the file you want to download
     let audio_ref = ref.child(path)
     let fileURL = try! FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: false).appendingPathComponent(local_path)
     
-    // Download in memory with a maximum allowed size of 1MB (1 * 1024 * 1024 bytes)
+    /// Download in memory with a maximum allowed size of 1MB (1 * 1024 * 1024 bytes)
     audio_ref.getData(maxSize: 1 * 1024 * 1024) { (data, error) in
         if let error = error {
+            
+            /// Error downloading file - synthesize word
             print(error)
+            synthesize_audio(of: word)
+            
         } else {
-
+            
+            /// Play the recovered audio file
             if let d = data {
-                print("YAY")
-                
                 do {
                     try d.write(to: fileURL)
                     player = try AVAudioPlayer(contentsOf: fileURL)
                     player.play()
                 } catch {
+                    /// Another error - synthesize
                     print("err in playing: ", error)
+                    synthesize_audio(of: word)
                 }
             }
             
         }
     }
-    
-    
-    
 }
